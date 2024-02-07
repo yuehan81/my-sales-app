990057a83d9df432d5137d6ae96f8c6fe0226c1b
Johan Sermon <yuehan81@gmail.com>
2024-02-07 14:50:33 +0100

    menu with routing and finished categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatSlideToggle,
+    HomeComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-sales-app';
+
 }
